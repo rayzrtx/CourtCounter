1c764bcbef32d72edc519a4f116dc71de5bd6ecf
Ray Linares <rayl77@gmail.com>
2017-11-29 13:05:43 -0600

    Clicking buttons now displays corresponding number of points of the button in team score

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreePoints(View view) {
+        displayForTeamA(3);
+    }
+
+    public void addTwoPoints(View view) {
+        displayForTeamA(2);
+    }
+
+    public void addOnePoint(View view) {
+        displayForTeamA(1);
+    }
+
+
 }
